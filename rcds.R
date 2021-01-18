6f7e16935cd6005d7afc99adb43022cd6e7ffd1c
esmoreido <moreido@mail.ru>
2021-01-18 13:27:42 +0300

    Empty error problem solved: dataset changed to dataset_short_name in wf_request list

--- rcds.R
+++ rcds.R
@@ -37,7 +37,7 @@ for(y in f.years){
     r[['month']] = f.months # запрашиваемые месяцы, генерируется последовательность строковых чисел с "0" перед единицами
     r[['day']] = f.days # запрашиваемые дни, генерируется последовательность строковых чисел с "0" перед единицами
     r[['time']] = f.hours # часы
-    r[['dataset']] = "reanalysis-era5-single-levels" # набор данных с сайта ()
+    r[['dataset_short_name']] = "reanalysis-era5-single-levels" # набор данных с сайта ()
     r[['target']] = paste0("era5_", y, "_Jan-Dec", ".nc")
     print(r)
     ncfile <- wf_request(user = UID,
@@ -58,7 +58,12 @@ request <- list(
   month = c("01", "02", "03", "04", "05", "06", "07"),
   day = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"),
   time = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"),
-  dataset = "reanalysis-era5-single-levels",
+  dataset_short_name = "reanalysis-era5-single-levels",
   target = "era5_2019_Jan-Jul_t2.nc"
 )
 
+ncfile <- wf_request(user = "10804",
+                     request = request,   
+                     transfer = TRUE,  
+                     path = getwd(),
+                     verbose = TRUE)
